356921cf5fc77eae9c5eb4ca97cc52146c86f89d
Murisi Tarusenga <murisit@gmail.com>
2019-01-11 08:13:41 +0200

    Added the necessary account types.

--- transactions.pl
+++ transactions.pl
@@ -104,6 +104,14 @@ account_type(accounts_receivable, asset).
 account_type(sales, revenue).
 account_type(cost_of_goods_sold, expense).
 account_type(stationary, expense).
+account_type(wages, expense).
+account_type(super_expense, expense).
+account_type(super_payable, liability).
+account_type(paygw_tax, liability).
+account_type(wages_payable, liability).
+account_type(motor_vehicles, asset).
+account_type(hirepurchase_truck, liability).
+account_type(hirepurchase_interest, expense).
 
 % Account isomorphisms. They are standard conventions in accounting.
 
