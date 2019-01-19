8470d07b4d405d22f3f48be1787eb456f696fb5d
Murisi Tarusenga <murisit@gmail.com>
2019-01-19 23:19:26 +0200

    Finished basic hire purchase arrangements. Not yet tested for missing variables.

--- HirePurchaseArrangements/hirepurchase.pl
+++ HirePurchaseArrangements/hirepurchase.pl
@@ -10,7 +10,7 @@ hp_arr_installment_period(hp_arrangement(_, _, _, Installment_Period, _, _), Ins
 
 hp_arr_installment_amount(hp_arrangement(_, _, _, _, Installment_Amount, _), Installment_Amount).
 
-hp_arr_interest(hp_arrangement(_, _, _, _, _, Interest), Interest).
+hp_arr_record_offset(hp_arrangement(_, _, _, _, _, Record_Offset), Record_Offset).
 
 % Predicates for asserting the fields of a hire purchase record
 
@@ -28,8 +28,8 @@ hp_rec_closing_balance(hp_record(_, _, _, _, _, Closing_Balance), Closing_Balanc
 
 % Predicate relating a hire purchase record to an agreement
 
-record_of(Record, Arrangement) :-
-	hp_rec_number(Record, 1),
+record_of_hp_arr(Arrangement, Record) :-
+	hp_rec_number(Record, Record_Number), hp_arr_record_offset(Arrangement, Record_Number),
 	hp_rec_opening_balance(Record, Cash_Price), hp_arr_cash_price(Arrangement, Cash_Price),
 	hp_rec_interest_rate(Record, Interest_Rate), hp_arr_interest_rate(Arrangement, Interest_Rate),
 	hp_arr_installment_period(Arrangement, Installment_Period),
@@ -37,24 +37,42 @@ record_of(Record, Arrangement) :-
 	hp_rec_interest_amount(Record, Interest_Amount),
 	hp_rec_installment_amount(Record, Installment_Amount), hp_arr_installment_amount(Arrangement, Installment_Amount),
 	Closing_Balance is Cash_Price + Interest_Amount - Installment_Amount,
-	hp_rec_closing_balance(Record, Closing_Balance).
-
-record_of(Record, Arrangement) :-
-	hp_rec_number(Record, Record_Number),
-	Prev_Record_Number is Record_Number - 1, hp_rec_number(Prev_Record, Prev_Record_Number),
-	record_of(Prev_Record, Arrangement),
-	hp_rec_closing_balance(Prev_Record, Opening_Balance), hp_rec_opening_balance(Record, Opening_Balance),
-	hp_rec_interest_rate(Record, Interest_Rate), hp_arr_interest_rate(Arrangement, Interest_Rate),
-	hp_arr_installment_period(Arrangement, Installment_Period),
-	Interest_Amount is Opening_Balance * Interest_Rate * Installment_Period / (100 * 12),
-	hp_rec_interest_amount(Record, Interest_Amount),
-	hp_rec_installment_amount(Record, Installment_Amount), hp_arr_installment_amount(Arrangement, Installment_Amount),
-	Closing_Balance is Opening_Balance + Interest_Amount - Installment_Amount,
 	hp_rec_closing_balance(Record, Closing_Balance),
-	Closing_Balance > 0.
+	Closing_Balance >= 0.
 
-range(X, L, H) :- X is L + 1, X < H.
-range(X, L, H) :- L1 is L + 1, L1 < H, range(X, L1, H).
+record_of_hp_arr(Arrangement, Record) :-
+	hp_arr_record_offset(Arrangement, Record_Offset),
+	New_Record_Offset is Record_Offset + 1,
+	hp_arr_record_offset(New_Arrangement, New_Record_Offset),
+	hp_arr_contract_number(Arrangement, Contract_Number), hp_arr_contract_number(New_Arrangement, Contract_Number),
+	hp_arr_cash_price(Arrangement, Cash_Price),
+	hp_arr_interest_rate(Arrangement, Interest_Rate), hp_arr_interest_rate(New_Arrangement, Interest_Rate),
+	hp_arr_installment_period(Arrangement, Installment_Period), hp_arr_installment_period(New_Arrangement, Installment_Period),
+	Interest_Amount is Cash_Price * Interest_Rate * Installment_Period / (100 * 12),
+	hp_arr_installment_amount(Arrangement, Installment_Amount), hp_arr_installment_amount(New_Arrangement, Installment_Amount),
+	New_Cash_Price is Cash_Price + Interest_Amount - Installment_Amount,
+	hp_arr_cash_price(New_Arrangement, New_Cash_Price),
+	New_Cash_Price >= 0,
+	record_of_hp_arr(New_Arrangement, Record).
 
-% hp_rec_number(Record, 1), record_of(Record, hp_arrangement(0, 5953.2, 13, 1, 200.47, in_arrears)).
+% Some more predicates for hire purchase arrangements
+
+records_of_hp_arr(Arrangement, Records) :- findall(Record, record_of_hp_arr(Arrangement, Record), Records).
+
+record_count_of_hp_arr(Arrangement, Record_Count) :-
+	findall(Record_Number, (record_of_hp_arr(Arrangement, Record), hp_rec_number(Record, Record_Number)),
+		Record_Numbers),
+	max_list(Record_Numbers, Record_Count).
+
+total_payment_of_hp_arr(Arrangement, Total_Payment) :-
+	findall(Installment_Amount, (record_of_hp_arr(Arrangement, Record), hp_rec_installment_amount(Record, Installment_Amount)),
+		Installment_Amounts),
+	sum_list(Installment_Amounts, Total_Payment).
+
+total_interest_of_hp_arr(Arrangement, Total_Interest) :-
+	findall(Interest_Amount, (record_of_hp_arr(Arrangement, Record), hp_rec_interest_amount(Record, Interest_Amount)),
+		Interest_Amounts),
+	sum_list(Interest_Amounts, Total_Interest).
+
+% record_of(hp_arrangement(0, 5953.2, 13, 1, 200.47, 1), Record).
 
