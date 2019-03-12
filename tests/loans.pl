3b170fc39f5f7240a4e35d4471fecbcec182a9c5
Murisi Tarusenga <murisit@gmail.com>
2019-03-12 08:03:25 +0200

    Added a test for loan computations where a computation opening balance is specified.

--- tests/loans.pl
+++ tests/loans.pl
@@ -95,10 +95,28 @@ findall(Summary,
 		absolute_day(date(2015, 5, 15), Lodgement_Day),
 		absolute_day(date(2015, 5, 30), Payment2_Day),
 		loan_agr_prepare(loan_agreement(0, 75000, 0, Lodgement_Day, Begin_Day, 7, 0, false,
-		[loan_repayment(Payment1_Day, 20000), loan_repayment(Payment2_Day, 8000)]), Prepared_Agreement),
+			[loan_repayment(Payment1_Day, 20000), loan_repayment(Payment2_Day, 8000)]), Prepared_Agreement),
 		loan_agr_summary(Prepared_Agreement, Summary)),
 	
 	[loan_summary(0, 55000, 5.95, 9834.923730309263, 28000, 0, 3230.7684931506847, 24769.231506849315, 50230.76849315068)]).
+
+% Summarize the information pertaining to the income years after a loan agreement is
+% made. The relevant information is that the first income year begins on date(2016, 7, 1).
+% The private company's lodgement day is date(2017, 5, 15). A payment of $3,000 is made
+% on date(2017, 7, 1). The income year of computation is 2018 and it opens with a balance
+% of $10,000. The loan has a term of 4 years.
+
+write("Is output for the loan summary information of a different agreement correct?"),
+
+findall(Summary,
+	(absolute_day(date(2016, 7, 1), Begin_Day),
+		absolute_day(date(2017, 5, 15), Lodgement_Day),
+		absolute_day(date(2017, 7, 1), Payment1_Day),
+		loan_agr_prepare(loan_agreement(0, 75000, 0, Lodgement_Day, Begin_Day, 4, 1, 10000,
+			[loan_repayment(Payment1_Day, 3000)]), Prepared_Agreement),
+		loan_agr_summary(Prepared_Agreement, Summary)),
+	
+	[loan_summary(1, 10000, 5.3, 3692.746389804068, 3000, 692.7463898040678, 371.0, 2629.0, 7371.0)]).
 	
 % See the loan summary schema in loans.pl .
 
