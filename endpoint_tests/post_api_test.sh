875eea9f64c0d90119b05e52fd318007b0d3c4a6
koo5 <kolman.jindrich@gmail.com>
2019-05-14 17:18:24 +0200

    ...

--- endpoint_tests/post_api_test.sh
+++ endpoint_tests/post_api_test.sh
@@ -1,7 +1,7 @@
-curl -s -X POST --data-binary "@$2" --header "Accept: application/xml" -o "response1.xml" \
+curl -s -X POST --data-binary "@$2" --header "Accept: application/xml" -o "tmp/response1.xml" \
 	"$1"
-tr -d '\040\011\012\015' < "$3" > "response2.xml"
-diff -w -s "response1.xml" "response2.xml" &>/dev/null
+tr -d '\040\011\012\015' < "$3" > "tmp/response2.xml"
+diff -w -s "tmp/response1.xml" "tmp/response2.xml" &>/dev/null
 if [ $? -eq 0 ]
 then
 	echo "Posting $2 to $1 produces expected response: $3."
@@ -9,6 +9,6 @@ else
 	echo "Posting $2 to $1 produces unexpected response."
 fi
 echo
-rm "response1.xml"
-rm "response2.xml"
+rm "tmp/response1.xml"
+rm "tmp/response2.xml"
 
