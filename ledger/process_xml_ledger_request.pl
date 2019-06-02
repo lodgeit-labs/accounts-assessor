a3d9af3ec64e7025d09099ef758610d65dfe0518
Schwitter <Rolf.Schwitter@mq.edu.au>
2019-06-02 15:59:26 +1000

    Started to use modules, some clean-up required.

---
+++ ledger/process_xml_ledger_request.pl
@@ -0,0 +1,241 @@
+% ===================================================================
+% Project:   LodgeiT
+% Module:    process_xml_ledger_request.pl  
+% Author:    Jindrich
+% Date:      2019-06-02
+% ===================================================================
+
+% -------------------------------------------------------------------
+% Modules
+% -------------------------------------------------------------------
+
+:- module(process_xml_ledger_request, [process_xml_ledger_request/2]).
+
+:- use_module(library(xpath)).
+:- use_module('../../lib/days', [format_date/2, parse_date/2, gregorian_date/2]).
+:- use_module('../../lib/ledger', [balance_sheet_at/8]).
+:- use_module('../../lib/statements', [preprocess_s_transactions/4]).
+
+% ------------------------------------------------------------------
+% process_xml_ledger_request/2
+% ------------------------------------------------------------------
+
+process_xml_ledger_request(_FileNameIn, DOM) :-
+   extract_default_bases(DOM, DefaultBases),
+   extract_action_taxonomy(DOM, ActionTaxonomy),
+   (
+      (
+         inner_xml(DOM, //reports/balanceSheetRequest/accountHierarchyUrl, [AccountHierarchyUrl]), 
+         fetch_account_hierarchy_from_url(AccountHierarchyUrl, AccountHierarchyDom), !
+      )
+      ;
+         load_xml('./taxonomy/account_hierarchy.xml', AccountHierarchyDom, [])
+   ),
+   extract_account_hierarchy(AccountHierarchyDom, AccountHierarchy),
+ 
+   findall(Transaction, extract_transactions(DOM, DefaultBases, Transaction), S_Transactions),
+
+   inner_xml(DOM, //reports/balanceSheetRequest/startDate, [BalanceSheetStartDateAtom]),
+   parse_date(BalanceSheetStartDateAtom, BalanceSheetStartAbsoluteDays),
+   inner_xml(DOM, //reports/balanceSheetRequest/endDate, [BalanceSheetEndDateAtom]),
+   parse_date(BalanceSheetEndDateAtom, BalanceSheetEndAbsoluteDays),
+
+   extract_exchange_rates(DOM, BalanceSheetEndAbsoluteDays, ExchangeRates),
+
+   preprocess_s_transactions(ExchangeRates, ActionTaxonomy, S_Transactions, Transactions),
+%print_term(preprocess_s_transactions(ExchangeRates, ActionTaxonomy, S_Transactions, Transactions),[]),
+   balance_sheet_at(ExchangeRates, AccountHierarchy, Transactions, DefaultBases, BalanceSheetEndAbsoluteDays, BalanceSheetStartAbsoluteDays, BalanceSheetEndAbsoluteDays, BalanceSheet),
+
+   pretty_term_string(S_Transactions, Message0),
+   pretty_term_string(Transactions, Message1),
+   pretty_term_string(ExchangeRates, Message1b),
+   pretty_term_string(ActionTaxonomy, Message2),
+   pretty_term_string(AccountHierarchy, Message3),
+   pretty_term_string(BalanceSheet, Message4),
+   atomic_list_concat([
+   	'S_Transactions:\n', Message0,'\n\n',
+   	'Transactions:\n', Message1,'\n\n',
+   	'Exchange rates::\n', Message1b,'\n\n',
+   	'ActionTaxonomy:\n',Message2,'\n\n',
+   	'AccountHierarchy:\n',Message3,'\n\n',
+   	'BalanceSheet:\n', Message4,'\n\n'],
+      DebugMessage),
+   display_xbrl_ledger_response(DebugMessage, BalanceSheetStartAbsoluteDays, BalanceSheetEndAbsoluteDays, BalanceSheet).
+
+% -----------------------------------------------------
+
+
+% this gets the children of an element with ElementXPath
+inner_xml(DOM, ElementXPath, Children) :-
+   xpath(DOM, ElementXPath, element(_,_,Children)).
+
+pretty_term_string(Term, String) :-
+   new_memory_file(X),
+   open_memory_file(X, write, S),
+   print_term(Term, [output(S)]),
+   close(S),
+   memory_file_to_string(X, String).
+
+extract_default_bases(DOM, Bases) :-
+   inner_xml(DOM, //reports/balanceSheetRequest/defaultUnitTypes/unitType, Bases).
+
+  
+fetch_account_hierarchy_from_url(AccountHierarchyUrl, AccountHierarchyDom) :-
+   http_get(AccountHierarchyUrl, AccountHierarchyXmlText, []),
+   store_xml_document('tmp/account_hierarchy.xml', AccountHierarchyXmlText),
+   load_xml('tmp/account_hierarchy.xml', AccountHierarchyDom, []).
+  
+% ------------------------------------------------------------------
+% extract_account_hierarchy/2
+%
+% Load Account Hierarchy terms from DOM
+% ------------------------------------------------------------------
+
+extract_account_hierarchy(AccountHierarchyDom, AccountHierarchy) :-   
+   % fixme when input format is agreed on
+   findall(Account, xpath(AccountHierarchyDom, //accounts, Account), Accounts),
+   findall(Link, (member(TopLevelAccount, Accounts), accounts_link(TopLevelAccount, Link)), AccountHierarchy).
+
+ 
+% yields all child-parent pairs describing the account hierarchy
+accounts_link(element(ParentName,_,Children), Link) :-
+   member(Child, Children), 
+   Child = element(ChildName,_,_),
+   (
+      % yield an account(Child, Parent) term for this child
+      Link = account(ChildName, ParentName)
+      ;
+      % recurse on the child
+      accounts_link(Child, Link)
+   ).
+
+extract_action_taxonomy(DOM, ActionTaxonomy) :-
+   findall(Action, xpath(DOM, //reports/balanceSheetRequest/actionTaxonomy/action, Action), Actions),
+   maplist(extract_action, Actions, ActionTaxonomy).
+   
+extract_action(In, transaction_type(Id, ExchangeAccount, TradingAccount, Description)) :-
+   inner_xml(In, id, [Id]),
+   inner_xml(In, description, [Description]),
+   inner_xml(In, exchangeAccount, [ExchangeAccount]),
+   inner_xml(In, tradingAccount, [TradingAccount]).
+   
+extract_exchange_rates(DOM, BalanceSheetEndDate, ExchangeRates) :-
+   findall(UnitValue, xpath(DOM, //reports/balanceSheetRequest/unitValues/unitValue, UnitValue), UnitValues),
+   maplist(extract_exchange_rate(BalanceSheetEndDate), UnitValues, ExchangeRates).
+   
+extract_exchange_rate(BalanceSheetEndDate, UnitValue, ExchangeRate) :-
+   ExchangeRate = exchange_rate(BalanceSheetEndDate, SrcCurrency, DestCurrency, Rate),
+   inner_xml(UnitValue, unitType, [SrcCurrency]),
+   inner_xml(UnitValue, unitValueCurrency, [DestCurrency]),
+   inner_xml(UnitValue, unitValue, [RateString]),
+   atom_number(RateString, Rate).
+
+% yield all transactions from all accounts one by one
+% these are s_transactions, the raw transactions from bank statements. Later each s_transaction will be preprocessed
+% into multiple transaction(..) terms.
+extract_transactions(DOM, DefaultBases, Transaction) :-
+   xpath(DOM, //reports/balanceSheetRequest/bankStatement/accountDetails, Account),
+   inner_xml(Account, accountName, [AccountName]),
+   inner_xml(Account, currency, [Currency]),
+   xpath(Account, transactions/transaction, T),
+   extract_transaction2(T, Currency, DefaultBases, AccountName, Transaction).
+
+extract_transaction2(T, Currency, DefaultBases, Account, ST) :-
+   xpath(T, debit, element(_,_,[DebitAtom])),
+   xpath(T, credit, element(_,_,[CreditAtom])),
+   xpath(T, transdesc, element(_,_,[Desc])),
+   xpath(T, transdate, element(_,_,[DateAtom])),
+   parse_date(DateAtom, AbsoluteDays),
+   atom_number(DebitAtom, Debit),
+   atom_number(CreditAtom, Credit),
+   Coord = coord(Currency, Debit, Credit),
+   ST = s_transaction(AbsoluteDays, Desc, [Coord], Account, Exchanged),
+   extract_exchanged_value(T, DefaultBases, Exchanged).
+
+extract_exchanged_value(T, DefaultBases, Exchanged) :-
+   % if unit type and count is specified, unifies Exchanged with a one-item vector with a coord with those values
+   % otherwise unifies Exchanged with bases(..) to trigger unit conversion later
+   % todo rewrite this with ->/2 ?
+   (
+      xpath(T, unitType, element(_,_,[UnitType])),
+      (
+         (
+            xpath(T, unit, element(_,_,[UnitCountAtom])),
+            %  If the user has specified both the unit quantity and type, then exchange rate
+            %  conversion and hence a target bases is unnecessary.
+            atom_number(UnitCountAtom, UnitCount),
+            Exchanged = vector([coord(UnitType, UnitCount, 0)]),!
+         )
+         ;
+         (
+            % If the user has specified only a unit type, then automatically do a conversion to that unit.
+            Exchanged = bases([UnitType])
+         )
+      ),!
+   )
+   ;
+   (
+      % If the user has not specified neither the unit quantity nor type, then automatically
+      %  do a conversion to the default bases.
+      Exchanged = bases(DefaultBases)
+   ).
+
+
+% -----------------------------------------------------
+% display_xbrl_ledger_response/4
+% -----------------------------------------------------
+
+display_xbrl_ledger_response(DebugMessage, BalanceSheetStartAbsoluteDays, BalanceSheetEndAbsoluteDays, BalanceSheetEntries) :-
+   format('Content-type: text/xml~n~n'), 
+   writeln('<?xml version="1.0"?>'),
+   writeln('<!--'),
+   writeln(DebugMessage),
+   writeln('-->'),
+   writeln('<xbrli:xbrl xmlns:xbrli="http://www.xbrl.org/2003/instance" xmlns:link="http://www.xbrl.org/2003/linkbase" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:iso4217="http://www.xbrl.org/2003/iso4217" xmlns:basic="http://www.xbrlsite.com/basic">'),
+   writeln('  <link:schemaRef xlink:type="simple" xlink:href="basic.xsd" xlink:title="Taxonomy schema" />'),
+   writeln('  <link:linkbaseRef xlink:type="simple" xlink:href="basic-formulas.xml" xlink:arcrole="http://www.w3.org/1999/xlink/properties/linkbase" />'),
+   writeln('  <link:linkBaseRef xlink:type="simple" xlink:href="basic-formulas-cross-checks.xml" xlink:arcrole="http://www.w3.org/1999/xlink/properties/linkbase" />'),
+
+   format_date(BalanceSheetEndAbsoluteDays, BalanceSheetEndDateString),
+   format_date(BalanceSheetStartAbsoluteDays, BalanceSheetStartDateString),
+   gregorian_date(BalanceSheetEndAbsoluteDays, date(BalanceSheetEndYear,_,_)),
+   
+   format( '  <context id="D-~w">\n', BalanceSheetEndYear),
+   writeln('    <entity>'),
+   writeln('      <identifier scheme="http://standards.iso.org/iso/17442">30810137d58f76b84afd</identifier>'),
+   writeln('    </entity>'),
+   writeln('    <period>'),
+   format( '      <startDate>~w</startDate>\n', BalanceSheetStartDateString),
+   format( '      <endDate>~w</endDate>\n', BalanceSheetEndDateString),
+   writeln('    </period>'),
+   writeln('  </context>'),
+
+   format_balance_sheet_entries(BalanceSheetEndYear, BalanceSheetEntries, [], UsedUnits, [], BalanceSheetLines),
+   maplist(write_used_unit, UsedUnits), 
+   atomic_list_concat(BalanceSheetLines, BalanceSheetLinesString),
+   writeln(BalanceSheetLinesString),
+   writeln('</xbrli:xbrl>'), nl, nl.
+
+format_balance_sheet_entries(_, [], UsedUnits, UsedUnits, BalanceSheetLines, BalanceSheetLines).
+
+format_balance_sheet_entries(BalanceSheetEndYear, Entries, UsedUnitsIn, UsedUnitsOut, BalanceSheetLinesIn, BalanceSheetLinesOut) :-
+   [entry(Name, Balances, Children)|EntriesTail] = Entries,
+   format_balances(BalanceSheetEndYear, Name, Balances, UsedUnitsIn, UsedUnitsIntermediate, BalanceSheetLinesIn, BalanceSheetLinesIntermediate),
+   format_balance_sheet_entries(BalanceSheetEndYear, Children, UsedUnitsIntermediate, UsedUnitsIntermediate2, BalanceSheetLinesIntermediate, BalanceSheetLinesIntermediate2),
+   format_balance_sheet_entries(BalanceSheetEndYear, EntriesTail, UsedUnitsIntermediate2, UsedUnitsOut, BalanceSheetLinesIntermediate2, BalanceSheetLinesOut).
+
+format_balances(_, _, [], UsedUnits, UsedUnits, BalanceSheetLines, BalanceSheetLines).
+
+format_balances(BalanceSheetEndYear, Name, [Balance|Balances], UsedUnitsIn, UsedUnitsOut, BalanceSheetLinesIn, BalanceSheetLinesOut) :-
+   format_balance(BalanceSheetEndYear, Name, Balance, UsedUnitsIn, UsedUnitsIntermediate, BalanceSheetLinesIn, BalanceSheetLinesIntermediate),
+   format_balances(BalanceSheetEndYear, Name, Balances, UsedUnitsIntermediate, UsedUnitsOut, BalanceSheetLinesIntermediate, BalanceSheetLinesOut).
+  
+format_balance(BalanceSheetEndYear, Name, coord(Unit, Debit, Credit), UsedUnitsIn, UsedUnitsOut, BalanceSheetLinesIn, BalanceSheetLinesOut) :-
+   union([Unit], UsedUnitsIn, UsedUnitsOut),
+   Balance is Debit - Credit,
+   format(string(BalanceSheetLine), '  <basic:~w contextRef="D-~w" unitRef="U-~w" decimals="INF">~w</basic:~w>\n', [Name, BalanceSheetEndYear, Unit, Balance, Name]),
+   append(BalanceSheetLinesIn, [BalanceSheetLine], BalanceSheetLinesOut).
+
+write_used_unit(Unit) :-
+   format('  <unit id="U-~w"><measure>~w</measure></unit>\n', [Unit, Unit]).
+   
---
+++ loan/process_xml_loan_request.pl
@@ -0,0 +1,141 @@
+% ===================================================================
+% Project:   LodgeiT
+% Module:    process_xml_loan_request.pl
+% Author:    Abdus Salam and Rolf Schwitter
+% Date:      2019-06-02
+% ===================================================================
+
+%--------------------------------------------------------------------
+% Modules
+%--------------------------------------------------------------------
+
+:- module(process_xml_loan_request, [process_xml_loan_request/2]).
+
+:- use_module('../../lib/loans', [loan_agr_summary/2]).
+:- use_module('../../lib/days',  [absolute_day/2]).
+
+
+% -------------------------------------------------------------------
+% process_xml_loan_request/2: loan-request.xml
+% -------------------------------------------------------------------
+
+process_xml_loan_request(FileNameIn, DOM) :-
+   (
+      FileNameIn  = 'tmp/loan-request.xml'
+   ->
+      FileNameOut = 'tmp/loan-response.xml';true
+   ),
+   xpath(DOM, //reports/loanDetails/loanAgreement/field(@name='Income year of loan creation', @value=CreationIncomeYear), E1),
+   xpath(DOM, //reports/loanDetails/loanAgreement/field(@name='Full term of loan in years', @value=Term), E2),
+   xpath(DOM, //reports/loanDetails/loanAgreement/field(@name='Principal amount of loan', @value=PrincipalAmount), E3),
+   xpath(DOM, //reports/loanDetails/loanAgreement/field(@name='Lodgment day of private company', @value=LodgementDate), E4),
+   xpath(DOM, //reports/loanDetails/loanAgreement/field(@name='Income year of computation', @value=ComputationYear), E5),   
+   (
+     xpath(DOM, //reports/loanDetails/loanAgreement/field(@name='Opening balance of computation', @value=OB), E6)
+     ->
+     OpeningBalance = OB
+   ;
+     OpeningBalance = -1
+   ),   
+   % need to handle empty repayments/repayment, needs to be tested
+   findall(loan_repayment(Date, Value), xpath(DOM, //reports/loanDetails/repayments/repayment(@date=Date, @value=Value), E7), LoanRepayments),
+   atom_number(ComputationYear, NIncomeYear),
+   convert_xpath_results(CreationIncomeYear,  Term,  PrincipalAmount,  LodgementDate,  ComputationYear,  OpeningBalance,  LoanRepayments,
+		         NCreationIncomeYear, NTerm, NPrincipalAmount, NLodgementDate, NComputationYear, NOpeningBalance, NLoanRepayments),   
+   loan_agr_summary(loan_agreement(0, NPrincipalAmount, NLodgementDate, NCreationIncomeYear, NTerm, 
+				   NComputationYear, NOpeningBalance, NLoanRepayments), Summary),
+   display_xml_loan_response(FileNameOut, NIncomeYear, Summary).
+
+   
+% -------------------------------------------------------------------
+% display_xml_loan_response/3
+% -------------------------------------------------------------------
+
+display_xml_loan_response(FileNameOut, IncomeYear, 
+                    loan_summary(_Number, OpeningBalance, InterestRate, MinYearlyRepayment, TotalRepayment,
+			         RepaymentShortfall, TotalInterest, TotalPrincipal, ClosingBalance)) :-
+   FileNameOut = 'tmp/loan-response.xml',
+   format('Content-type: text/xml~n~n'), 
+   writeln('<?xml version="1.0"?>'), nl,   
+   writeln('<LoanSummary xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'),
+   format('<IncomeYear>~w</IncomeYear>~n', IncomeYear),
+   format('<OpeningBalance>~w</OpeningBalance>~n', OpeningBalance),
+   format('<InterestRate>~w</InterestRate>~n', InterestRate),
+   format('<MinYearlyRepayment>~w</MinYearlyRepayment>~n', MinYearlyRepayment),
+   format('<TotalRepayment>~w</TotalRepayment>~n', TotalRepayment),
+   format('<RepaymentShortfall>~w</RepaymentShortfall>~n', RepaymentShortfall),
+   format('<TotalInterest>~w</TotalInterest>~n', TotalInterest),
+   format('<TotalPrincipal>~w</TotalPrincipal>~n', TotalPrincipal),
+   format('<ClosingBalance>~w</ClosingBalance>~n', ClosingBalance),
+   write('</LoanSummary>'), nl.
+   
+
+% ===================================================================
+% Various helper predicates
+% ===================================================================
+
+% -------------------------------------------------------------------
+% convert_xpath_results/14 
+% -------------------------------------------------------------------
+
+convert_xpath_results(CreationIncomeYear,  Term,  PrincipalAmount,  LodgementDate,  ComputationYear,  OpeningBalance,  LoanRepayments,
+		      NCreationIncomeYear, NTerm, NPrincipalAmount, NLodgementDate, NComputationYear, NOpeningBalance, NLoanRepayments) :-
+   generate_absolute_days(CreationIncomeYear, LodgementDate, LoanRepayments, NCreationIncomeYear, NLodgementDate, NLoanRepayments),
+   compute_opening_balance(OpeningBalance, NOpeningBalance),
+   calculate_computation_year(ComputationYear, CreationIncomeYear, NComputationYear),
+   atom_number(PrincipalAmount, NPrincipalAmount),
+   atom_number(Term, NTerm).
+
+generate_absolute_days(CreationIncomeYear, LodgementDate, LoanRepayments, NCreationIncomeYear, NLodgementDay, NLoanRepayments) :-
+   generate_absolute_day(creation_income_year, CreationIncomeYear, NCreationIncomeYear),
+   generate_absolute_day(lodgement_date, LodgementDate, NLodgementDay),
+   generate_absolute_day(loan_repayments, LoanRepayments, NLoanRepayments).
+     
+generate_absolute_day(creation_income_year, CreationIncomeYear, NCreationIncomeYear) :-
+   atom_number(CreationIncomeYear, CreationIncomeYearNumber),
+   absolute_day(date(CreationIncomeYearNumber, 7, 1), NCreationIncomeYear).
+
+generate_absolute_day(lodgement_date, LodgementDate, NLodgementDay) :-
+   generate_absolute_day_from_date_string(LodgementDate, NLodgementDay).
+
+generate_absolute_day(loan_repayments, [], []).
+
+generate_absolute_day(loan_repayments, [loan_repayment(Date, Value)|Rest1], [loan_repayment(NDate, NValue)|Rest2]) :-
+   generate_absolute_day_from_date_string(Date, NDate),
+   atom_number(Value, NValue),
+   generate_absolute_day(loan_repayments, Rest1, Rest2).
+
+   
+generate_absolute_day_from_date_string(DateString, AbsoluteDay) :-
+   split_string(DateString, "-", "", DateList),
+   DateList = [YearString, MonthString, DayString],	
+   atom_number(YearString, Year),
+   atom_number(MonthString, Month),
+   atom_number(DayString, Day),
+   absolute_day(date(Year, Month, Day), AbsoluteDay).   
+	
+	
+% ----------------------------------------------------------------------
+% compute_opening_balance/2
+% ----------------------------------------------------------------------
+
+compute_opening_balance(OpeningBalance, NOpeningBalance) :-
+   (
+     OpeningBalance = -1
+     ->
+     NOpeningBalance = false
+   ;
+     atom_number(OpeningBalance, NOpeningBalance)
+   ).
+
+% ----------------------------------------------------------------------
+% calculate_computation_year/3
+%
+% Computation year is calculated as it is done in:
+%	- ConstructLoanAgreement function (PrologEngpoint/LoanController.cs)
+% ----------------------------------------------------------------------
+
+calculate_computation_year(ComputationYear, CreationIncomeYear, NComputationYear) :-
+   atom_number(ComputationYear, NCY),
+   atom_number(CreationIncomeYear, NCIY),
+   NComputationYear is NCY - NCIY - 1.
