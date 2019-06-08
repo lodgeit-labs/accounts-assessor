71bd3429ee885cb6e68f87c1ed28dcc4ce0d6126
Schwitter <Rolf.Schwitter@mq.edu.au>
2019-06-08 18:02:03 +1000

    Updated
    store_xml_document/2 is still part of this file, since it is used by the ledger; but this predicate can be moved to the module: "process_xml_ledger_request.pl"

---
+++ prolog_server/process_data.pl
@@ -0,0 +1,49 @@
+% ===================================================================
+% Project:   LodgeiT
+% File:      process_data.pl
+% Author:    Rolf Schwitter
+% Date:      2019-06-08
+% ===================================================================
+
+% -------------------------------------------------------------------
+% Modules
+% -------------------------------------------------------------------
+
+:- use_module(library(xpath)).
+:- use_module(loan/process_xml_loan_request, [process_xml_loan_request/2]).
+:- use_module(ledger/process_xml_ledger_request, [process_xml_ledger_request/2]).   	  
+
+
+% -------------------------------------------------------------------
+% process_data/2
+% -------------------------------------------------------------------
+
+process_data(FileName, Path) :-
+   load_xml(Path, DOM, []),
+   process_xml_request(FileName, DOM).
+   % format('Content-type: text/plain~n~n'),
+   % writeq(DOM),
+   % write(FileName),
+   % write(Path).
+
+   
+% -------------------------------------------------------------------
+% process_xml_request/2
+% -------------------------------------------------------------------
+
+process_xml_request(FileName, DOM) :-
+   (
+      process_xml_loan_request(FileName, DOM)
+   ;
+      process_xml_ledger_request(FileName, DOM)
+   ).
+     
+
+% -------------------------------------------------------------------
+% store_xml_document/2
+% -------------------------------------------------------------------
+
+store_xml_document(FileName, XML) :-
+   open(FileName, write, Stream),
+   write(Stream, XML),
+   close(Stream).
