04b91e45d7a7dd27c882ac14f69b6654528d12d2
salamt2 <salam.t2@gmail.com>
2019-07-26 18:06:52 +1000

    updated depreciation request processing to get the computed value from depreciation_computation.pl

--- prolog_server/depreciation/process_xml_depreciation_request.pl
+++ prolog_server/depreciation/process_xml_depreciation_request.pl
@@ -1,7 +1,7 @@
 % ===================================================================
 % Project:   LodgeiT
 % Module:    process_xml_depreciation_request.pl
-% Date:      2019-07-24
+% Date:      2019-07-26
 % ===================================================================
 
 %--------------------------------------------------------------------
@@ -11,7 +11,11 @@
 :- module(process_xml_depreciation_request, [process_xml_depreciation_request/2]).
 
 :- use_module(library(xpath)).
-:- use_module('../../lib/utils', [inner_xml/3, write_tag/2]).
+:- use_module('../../lib/utils', [inner_xml/3, write_tag/2, fields/2]).
+:- use_module('../../lib/days', [parse_date/2]).
+:- use_module('../../lib/depreciation_computation', [
+		written_down_value/5, 
+		depreciation_between_two_dates/6]).
 
 
 % -------------------------------------------------------------------
@@ -31,27 +35,62 @@ process_xml_depreciation_request(_, DOM) :-
 
 
 process(DOM) :-
-	inner_xml(DOM, //depreciation_request_written_down_value/type, [Type]),
-	inner_xml(DOM, //depreciation_request_written_down_value/cost/unit, [CostUnit]),
-	inner_xml(DOM, //depreciation_request_written_down_value/cost/value, [CostValue]),
-	inner_xml(DOM, //depreciation_request_written_down_value/invest_in_date, [InvestInDate]),
-	inner_xml(DOM, //depreciation_request_written_down_value/request_date, [RequestDate]),
+	process_basic_values(DOM, Type, Invest_In_Date_In, Request_Date_In, Method, Cost_Unit, Cost_Value_In),	
+	
+	xpath(DOM, //reports/depreciation_request_written_down_value/rates, Depreciation_Rates_DOM),	
+	findall(depreciation_rate(Asset, Year, Value), 
+				(
+					process_depreciation_rate(Depreciation_Rates_DOM, Asset, Year_In, Value_In),
+					atom_number(Year_In, Year),
+					atom_number(Value_In, Value)
+				), 
+				Depreciation_Rates),
+	
+	writeln('<!--'),
+	parse_date(Invest_In_Date_In, Invest_In_Date),
+	parse_date(Request_Date_In, Request_Date),
+	atom_number(Cost_Value_In, Cost_Value),
+	% fixme: description and account values are given here. finally we have to extract these values and update it
+	Transaction = transaction(Invest_In_Date, 'TestDescription', motor_vehicles, t_term(Cost_Value, _)),	
+	depreciation_between_two_dates(Transaction, Invest_In_Date, Request_Date, Method, Depreciation_Rates, Depreciation_Value),
+	% written_down_value(Transaction, Request_Date, Method, Depreciation_Rates, Written_Down_Value),	
+	writeln('-->'),
 	
 	write_tag('type', Type),	
 	writeln('<cost>'),
-	write_tag('unit', CostUnit),
-	write_tag('value', CostValue),
+	write_tag('unit', Cost_Unit),
+	write_tag('value', Cost_Value),
 	writeln('</cost>'),	
-	
-	write_tag('invest_in_date', InvestInDate),
-	write_tag('request_date', RequestDate),
-	
+	write_tag('invest_in_date', Invest_In_Date_In),
+	write_tag('request_date', Request_Date_In),	
 	writeln('<written_down_value>'),
-	write_tag('unit', CostUnit),
-	% fixme: need to compute the value using the depreciation calculator
-	DepreciationValue = 123,
-	write_tag('value', DepreciationValue),
+	write_tag('unit', Cost_Unit),
+	write_tag('value', Depreciation_Value),
 	writeln('</written_down_value>').
 
-   
+
+process_basic_values(DOM, Type, Invest_In_Date, Request_Date, Method, Cost_Unit, Cost_Value) :-
+	xpath(DOM, //reports/depreciation_request_written_down_value, Depreciation_Request_Values),
+	fields(Depreciation_Request_Values, [
+		type, Type,		
+		invest_in_date, Invest_In_Date,
+		request_date, Request_Date,
+		method, Method
+	]),	
+	
+	xpath(DOM, //reports/depreciation_request_written_down_value/cost, Depreciation_Request_Cost_Values),
+	fields(Depreciation_Request_Cost_Values, [
+		unit, Cost_Unit,		
+		value, Cost_Value
+	]).
+
+
+process_depreciation_rate(Depreciation_Rates_DOM, Asset, Year, Value) :-
+	xpath(Depreciation_Rates_DOM, //depreciation_rate, Depreciation_Rate),		
+	fields(Depreciation_Rate, [
+		asset, Asset,
+		year, Year,
+		value, Value
+	]).
+
 
