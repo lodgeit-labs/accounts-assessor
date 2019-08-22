81c0c6425a46f3e222893de82c7ca532a4777f2c
stoopkid <bdavis2347@gmail.com>
2019-08-22 12:55:40 -0400

    issue #84, fixed  warning

--- xbrl/account_hierarchy/src/venv-wrapper.py
+++ xbrl/account_hierarchy/src/venv-wrapper.py
@@ -6,12 +6,11 @@ import subprocess
 # like:
 # > python3 venv-wrapper.py "; echo \"arbitrary shell command execution\""
 def main():
-	lodgeit_root = "/home/sfi/t-rex/lodgeit2/lodgeit"
-	script_root = lodgeit_root + "/xbrl/account_hierarchy"
+	script_root = "../xbrl/account_hierarchy"
 	activate_path = script_root + "/venv/bin/activate"
 	script_path = script_root + "/src/main.py"
 	python_command = "python3 {0} {1}".format(script_path, " ".join(sys.argv[1:]))
-	full_command = "pwd; echo ""; source {0} ; {1} ; deactivate".format(activate_path, python_command)
+	full_command = "source {0} ; {1} ; deactivate".format(activate_path, python_command)
 
 	output = subprocess.Popen(full_command, stdout=subprocess.PIPE, shell=True, executable="/bin/bash").stdout.read().decode("utf-8")
 	print(output)
