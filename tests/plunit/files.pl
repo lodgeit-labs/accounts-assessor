4111aaf6560b23faf73ec7cf257a644018baec42
koo5 <kolman.jindrich@gmail.com>
2019-09-16 23:31:48 +0200

    fix

--- tests/plunit/files.pl
+++ tests/plunit/files.pl
@@ -8,7 +8,7 @@ test(0) :-
     absolute_file_name(my_static('default_action_taxonomy.xml'), _,
                        [ access(read) ]),
     absolute_file_name(my_tests('endpoint_tests'), _,
-                       [ access(directory) ]),
+                       [ file_type(directory) ]),
 	catch(
 		(
 			absolute_file_name(my_tests('non_existent_file.which_really_should_not_exist'), _, [ access(read) ]),
