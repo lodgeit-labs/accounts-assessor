18791f6081b0eb208fab0417ecb7556185db9ed4
koo5 <kolman.jindrich@gmail.com>
2019-11-25 22:51:25 +0100

    improved test runner and updated testcases

---
+++ python/src/structural_xmldiff.py
@@ -0,0 +1,4 @@
+import os, sys
+
+os.system('../python/venv/bin/xmldiff ' + sys.argv[1] + ' ' + sys.argv[2] + """  | grep -v -F '[update-text-after, /' | grep -v -F '[insert-comment, /' | grep -v -F '/comment()['  """)
+
