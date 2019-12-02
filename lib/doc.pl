13790f60df33eb3a382a1ba4b2cc7254ca138eed
koo5 <kolman.jindrich@gmail.com>
2019-12-02 09:16:02 +0100

    ...

--- lib/doc.pl
+++ lib/doc.pl
@@ -1,30 +1,14 @@
 :- module(_, [doc/3, doc_add/3, doc_new_theory/1, doc_new_uri/1]).
-
+:- use_module(doc_core, [doc/3, doc_add/3]).
 :- use_module('files', []).
-
 :- use_module(library(semweb/rdf11)).
-:- use_module(library(debug)).
-
-:- dynamic doc_data/4.
 
 :- rdf_register_prefix(l, 'https://lodgeit.net.au#').
 :- rdf_register_prefix(livestock, 'https://lodgeit.net.au/livestock#').
 
-
-:- rdf_meta doc_add(r,r,r).
-
-:- debug(doc).
-
-doc_add(S,P,O) :-
-	files:my_request_tmp_dir(R),
-	debug(doc, '~q~n', [doc_data(R,S,P,O)]),
-	assertz(doc_data(R,S,P,O)).
-
-:- rdf_meta doc(r,r,r).
-
-doc(S,P,O) :-
-	files:my_request_tmp_dir(D),
-	doc_data(D,S,P,O).
+/*
+helper predicates
+*/
 
 doc_new_theory(T) :-
 	doc_new_uri(T),
