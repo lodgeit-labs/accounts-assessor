423a02b8989f55da8cc1527b09bad72a659e83b9
koo5 <kolman.jindrich@gmail.com>
2020-01-09 10:28:25 +0100

    gl viewer fixup.......

--- frontend_server/endpoints_gateway/static/README.txt
+++
@@ -1,6 +0,0 @@
-Account Hierarchy Taxonomy is currently in XML format. Ultimate goal is to convert it to XBRL Taxonomy (Schema).
-
-This Account Hierarchy is loaded in the process_xml_ledger_request.pl while handling the ledger request to generate the ledger response.
-The predicate process_xml_ledger_request loads the taxonomoy, which is used to preprocess everything and ultimately structure the response generated in balance_sheet_at predicate in ledger.pl.
-
-see also accounts.pl
--- lib/ledger.pl
+++ lib/ledger.pl
@@ -69,7 +69,7 @@ process_ledger(
 	maplist(check_transaction_account(Accounts), Transactions_With_Livestock),
 
 	Static_Data2 = Static_Data0.put(end_date, Processed_Until).put(transactions, Transactions_With_Livestock),
-	generate_gl_data(
+	gl_export(
 		Static_Data2, 
 		Processed_S_Transactions, 
 		/* list of lists */
@@ -114,47 +114,6 @@ preprocess_until_error(Static_Data0, Prepreprocessed_S_Transactions, Preprocesse
 		)
 	).
 
-generate_gl_data(Sd, Processed_S_Transactions, Transactions0, Livestock_Transactions, Report_Dict) :-
-	/* Outputs list is lists of generated transactions */
-	append(Transactions0, [Livestock_Transactions], Processing_Results),
-	/* Sources list is all the s_transactions + livestock */
-	append(Processed_S_Transactions, ['livestock'], Sources),
-	maplist(make_gl_entry(Sd), Sources, Processing_Results, Report_Dict).
-
-make_gl_entry(Sd, Source, Transactions, Entry) :-
-	Entry = _{source: S, transactions: T},
-	(
-		atom(Source)
-	-> 
-		S = Source
-	; 
-		(
-			s_transaction_to_dict(Source, S0),
-			/* currently this is converted at transaction date */
-			s_transaction_with_transacted_amount(Sd, S0, S)
-		)
-	),
-	maplist(transaction_to_dict, Transactions, T0),
-	maplist(transaction_with_converted_vector(Sd), T0, T).
-	
-s_transaction_with_transacted_amount(Sd, D1, D2) :-
-	D2 = D1.put([
-		report_currency_transacted_amount_converted_at_transaction_date=ConvertedA,report_currency_transacted_amount_converted_at_balance_date=ConvertedB]),
-	vec_change_bases(Sd.exchange_rates, D1.date, Sd.report_currency, D1.vector, ConvertedA),
-	vec_change_bases(Sd.exchange_rates, Sd.end_date, Sd.report_currency, D1.vector, ConvertedB).
-
-transaction_with_converted_vector(Sd, Transaction, Transaction_Converted) :-
-	Transaction_Converted = Transaction.put([
-		vector_converted_at_transaction_date=Vector_ConvertedA,
-		vector_converted_at_balance_date=Vector_ConvertedB
-	]),
-	vec_change_bases(Sd.exchange_rates, Transaction.date, Sd.report_currency, Transaction.vector, Vector_ConvertedA),
-	vec_change_bases(Sd.exchange_rates, Sd.end_date, Sd.report_currency, Transaction.vector, Vector_ConvertedB).
-		
-trial_balance_ok(Trial_Balance_Section) :-
-	Trial_Balance_Section = entry(_, Balance, [], _),
-	maplist(coord_is_almost_zero, Balance).
-	
 gather_ledger_warnings(S_Transactions, Start_Date, End_Date, Warnings) :-
 	(
 		find_s_transactions_in_period(S_Transactions, Start_Date, End_Date, [])
