32fc6e20ffafeecef241e38e1fc0fede8d9b0534
koo5 <kolman.jindrich@gmail.com>
2020-02-21 11:13:12 +0100

    pyco2.pl..

--- misc/pyco2.pl
+++ misc/pyco2.pl
@@ -1,6 +1,12 @@
 :- use_module(library(clpfd)).
 :- op(900,xfx,<=).
 
+list_to_u([], nil).
+list_to_u([H|T], Cell) :-
+	proof(fr(Cell,H,Cell2)),
+	list_to_u(T, Cell2).
+
+
 pyco0_rule(
 	'list cell helper',
 	[fr(L,F,R)] <=
@@ -67,6 +73,21 @@ pyco0_rule(
 		nl
 	]).
 
+pyco0_rule(
+	'test query',
+	[test_statement1] <=
+	[
+		End = 9,
+		s_transaction_day(T1, 1),
+		s_transaction_day(T2, 2),
+		s_transaction_day(T5, 5),
+		s_transaction_day(T10, 10),
+		s_transactions_up_to(End, All, Capped),
+		writeq('Capped:'),writeq(Capped),nl,
+		writeq('All:'),writeq(All),nl
+	]) :-
+		list_to_u([T1,T2,T5], Capped).
+
 matching_rule(Query, Body_items) :-
 	pyco0_rule(_, Head_items <= Body_items),
 	member(Query, Head_items).
@@ -82,3 +103,46 @@ proof(Query) :-
 
 :- proof(test_statement0).
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+?x a response
+?request result ?result
+?response result ?result
+
+=====
+
+?sts0 prepreprocess ?sts1
+?sts1 preprocess ?txs
+
+
+
+{?sts0 prepreprocess ?sts1} <=
+{
+    ?sts0 first ?st0
+    ?st0 action_verb "livestock_sell"
+
+    .....
+
+    ?sts0 rest ?stsr
+    ?stsr prepreprocess ?sts1r.
+
+two approaches to optimization:
+    follow the data:
+        ?txs are bound, so call ?sts1 preprocess ?txs first
+    ep-yield earlier:
+        as soon as we're called with only vars?
+
+
+*/
