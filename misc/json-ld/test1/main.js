d6d2cd6468a9f245bd1d9086cd00003ed8a075f5
koo5 <kolman.jindrich@gmail.com>
2020-10-02 15:20:27 +0200

    works..

--- misc/json-ld/test1/main.js
+++ misc/json-ld/test1/main.js
@@ -3,9 +3,7 @@
 const processor = require('./processor.js')
 var jl = require('jsonld');
 
-const frame =
-{
-	"@context": {
+const ctx = {
 		"@base": "https://rdf.lodgeit.net.au/v1/",
 		"xsd": "http://www.w3.org/2001/XMLSchema#",
 		"rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
@@ -18,19 +16,56 @@ const frame =
 		"smsf_distribution": "https://rdf.lodgeit.net.au/v1/calcs/smsf/distribution#",
 		"smsf_distribution_ui": "https://rdf.lodgeit.net.au/v1/calcs/smsf/distribution_ui#",
 		//"excel:multiple_sheets_allowed":{"@id"}
-	},
-	//"@type":"excel:sheet_set",
-	"@id": "calcs/hp/ui#hirepurchase_calculator_sheets",
-
 };
+
 
 (async () => {
 	var doc = await processor.load_n3('RdfTemplates2.n3');
-	doc = await jl.compact(doc,{});
+	//doc = await jl.compact(doc,{});
 	//console.log(doc);
-	var r = await processor.frame(doc, frame);
-	console.log(JSON.stringify(r, null, 2))
+
+
+	const idd = async (frame) =>
+	{
+		frame["@context"] = ctx;
+		const fr = await processor.frame(doc, frame);
+		//console.log(fr)
+		return ids_to_keys(fr);
+	}
+
+	const result = {
+		sheet_sets: await idd(
+			{
+				"@type": "excel:sheet_set",
+				"excel:sheets":
+					{
+						"@list": [
+							{
+								"@embed": "@never"
+							}
+						]
+					}
+			}
+			),
+		sheet_types: await idd(
+			{
+				"@type": "excel:sheet_type"
+			}
+			),
+		};
+	console.log(JSON.stringify(result, null, 2))
 })();
+
+function ids_to_keys(doc)
+{
+	result = {};
+	doc['@graph'].forEach((x) =>
+		{
+			result[x['@id']] = x;
+		}
+	);
+	return result;
+}
 
 /*
 goals:
