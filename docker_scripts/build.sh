ca761cf6f105ad3417d5d17fd1f1f04889353327
koo5 <kolman.jindrich@gmail.com>
2020-11-29 16:10:15 +0100

    agraph secrets...

--- docker_scripts/build.sh
+++ docker_scripts/build.sh
@@ -7,7 +7,7 @@ docker pull franzinc/agraph:v7.0.0
 
 git status > static/docs/git_info.txt; and git log >> static/docs/git_info.txt;
 
-and cp ../../secrets*.json .; 
+and cp -r ../../secrets/ .; 
 
 and docker build -t  "koo5/internal-workers$PP"   -f "internal_workers/Dockerfile$INTERNAL_WORKERS_DOCKERFILE_CHOICE" . ; 
 and docker build -t  "koo5/internal-services$PP"  -f "internal_services/Dockerfile" . ; 
--- docker_scripts/deploy_stack.sh
+++ docker_scripts/deploy_stack.sh
@@ -3,6 +3,6 @@
 set PP $argv[1]
 set COMPOSE_FILE $argv[2]
 
-env PP="$PP" docker stack deploy --prune --compose-file "docker-stack$COMPOSE_FILE.yml" "robust$PP"
+env PP="$PP" docker stack deploy --prune --compose-file "$COMPOSE_FILE" "robust$PP"
 
 docker stack ps "robust$argv[1]"
