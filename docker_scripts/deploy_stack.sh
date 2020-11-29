83b9b2015dba25a957f0f037a955cd86a384d97c
koo5 <kolman.jindrich@gmail.com>
2020-11-29 11:03:34 +0100

    docker multiportizing...

--- docker_scripts/deploy_stack.sh
+++ docker_scripts/deploy_stack.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 
+set PP $argv[1]
+set INTERNAL_WORKERS_DOCKERFILE_CHOICE $argv[2]
 
-env PP="$argv[1]" docker stack deploy --prune --compose-file docker-stack.yml "robust$argv[1]"
+env PP="$PP" docker stack deploy --prune --compose-file "docker-stack$INTERNAL_WORKERS_DOCKERFILE_CHOICE.yml" "robust$PP"
 
 docker stack ps "robust$argv[1]"
