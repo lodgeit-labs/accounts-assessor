6c392b91679a100f115a9d78b317da860f9a85dc
koo5 <kolman.jindrich@gmail.com>
2021-01-09 19:26:00 +0100

    ..

--- docker_scripts/build.sh
+++ docker_scripts/build.sh
@@ -11,12 +11,13 @@ set PP $argv[1]
 set MODE $argv[2]
 
 # rdf koo:notes:agraph
-docker pull franzinc/agraph:v7.0.0
+#docker pull franzinc/agraph:v7.0.0
 
 
 git status > static/docs/git_info.txt; and git log >> static/docs/git_info.txt;
 
 and docker build -t  "koo5/flower$PP"             -f "../docker_scripts/flower/Dockerfile" . ; 
+and docker build -t  "koo5/apache$PP"             -f "../docker_scripts/apache/Dockerfile" . ; 
 and docker build -t  "koo5/agraph$PP"             -f "../docker_scripts/agraph/Dockerfile" . ; 
 and docker build -t  "koo5/internal-workers$PP"   -f "internal_workers/Dockerfile$MODE" . ; 
 and docker build -t  "koo5/internal-services$PP"  -f "internal_services/Dockerfile$MODE" . ; 
--- docker_scripts/follow_logs.sh
+++ docker_scripts/follow_logs.sh
@@ -3,6 +3,8 @@
 set PP $argv[1]
 
 docker service logs -f robust"$PP"_agraph  &
+docker service logs -f robust"$PP"_flower  &
+docker service logs -f robust"$PP"_apache  &
 docker service logs -f robust"$PP"_frontend-server  &
 docker service logs -f robust"$PP"_internal-workers &
 docker service logs -f robust"$PP"_internal-services
