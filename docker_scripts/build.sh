ea1872bb0741dbaa464a1b78197ce3cc6e9a0109
koo5 <kolman.jindrich@gmail.com>
2021-07-27 14:34:31 +0200

    reorg..

--- docker_scripts/build.sh
+++ docker_scripts/build.sh
@@ -18,11 +18,23 @@ set MODE $argv[2]
 ../docker_scripts/git_info.fish
 
 
+and echo "";
+and echo "flower...";
 and docker build -t  "koo5/flower$PP"             -f "../docker_scripts/flower/Dockerfile" . ; 
+and echo "";
+and echo "apache...";
 and docker build -t  "koo5/apache$PP"             -f "../docker_scripts/apache/Dockerfile" . ; 
+and echo "";
+and echo "agraph...";
 and docker build -t  "koo5/agraph$PP"             -f "../docker_scripts/agraph/Dockerfile" . ; 
+and echo "";
+and echo "internal-workers...";
 and docker build -t  "koo5/internal-workers$PP"   -f "internal_workers/Dockerfile$MODE" . ; 
+and echo "";
+and echo "internal-services...";
 and docker build -t  "koo5/internal-services$PP"  -f "internal_services/Dockerfile$MODE" . ; 
+and echo "";
+and echo "frontend-server...";
 and docker build -t  "koo5/frontend-server$PP"    -f "frontend_server/Dockerfile$MODE" . ; 
 
 and echo "ok"
