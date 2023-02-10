a69e538d11ed3582359fff0968889bbe897072ff
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-02-10 01:28:18 +0100

    alright, terminating tasks from the gui is also technically possible

--- tests2/runner/runner/execution_summary_example.py
+++ tests2/runner/runner/execution_summary_example.py
@@ -29,16 +29,39 @@ import luigi
 
 
 class Bar(luigi.Task):
+	accepts_messages = True
+	should_die = False
+
 	num = luigi.IntParameter()
 	ts = luigi.Parameter('_')
 
+	def proces_msgs(self):
+		if not self.scheduler_messages.empty():
+			self.process_msg(self.scheduler_messages.get())
+
+	def process_msg(self, msg):
+		print(msg)
+		if msg.content == "die":
+			self.should_die = True
+			msg.respond("okay")
+		else:
+			msg.respond("unknown message")
+
+	def check_die(self):
+		self.proces_msgs()
+		return self.should_die
+
 	def run(self):
-		print('sleep 10s..');time.sleep(10)
-		print('sleep 10s..');time.sleep(10)
-		print('sleep 10s..');time.sleep(10)
-		print('sleep 10s..');time.sleep(10)
-		print('sleep 10s..');time.sleep(10)
-		print('sleep 10s..');time.sleep(10)
+		print('sleep 20s..');time.sleep(20)
+		if self.check_die(): return
+		print('sleep 20s..');time.sleep(20)
+		if self.check_die(): return
+		print('sleep 20s..');time.sleep(20)
+		if self.check_die(): return
+		print('sleep 20s..');time.sleep(20)
+		if self.check_die(): return
+		print('sleep 20s..');time.sleep(20)
+		if self.check_die(): return
 		self.output().open('w').close()
 
 	def output(self):
