311554bffce0d45fcd38c6958e7a1443b3d69576
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-02-26 00:57:18 +0100

    ..

--- tests2/runner/runner/tests2.py
+++ tests2/runner/runner/tests2.py
@@ -1,7 +1,11 @@
 import datetime
+import shutil
+
 import luigi
 import luigi.contrib.postgres
 import pathlib
+import json
+
 
 
 class Dummy(luigi.Task):
@@ -11,46 +15,104 @@ class Dummy(luigi.Task):
 		return False
 
 
-def	test_dir(session, permutation):
-	return session / permutation.dir / ('debug' if permutations.debug else 'nodebug')
+
+
+
+class Result(luigi.Task):
+	sess = luigi.parameter.PathParameter()
+	test = luigi.parameter.DictParameter()
+
+
+	def run(self):
+		inputs = self.copy_inputs()
+		self.run_request(inputs)
+
+
+	def copy_inputs(self):
+		request_files_dir: pathlib.Path = self.test.path / 'inputs'
+		request_files_dir.mkdir()
+		files = []
+		input_file: pathlib.Path
+		for input_file in sorted(filter(lambda x: not x.is_dir(), (test.suite / test.dir).glob('*'))):
+			shutil.copyfile(input_file, request_files_dir)
+			files.append(request_files_dir / input_file.name)
+		return files
+
+
+
+
+	def output(self):
+		return luigi.LocalTarget(self.test.path / 'outputs')
+
+
+
+
+
+class Evaluation(luigi.Task):
+	test = luigi.parameter.DictParameter()
+
+
+	def requires(self):
+		return Result(self.test)
+
+
+	def run(self):
+		with self.output().open('w') as out:
+			json.load(self.input() /)
+				summary += evaluation
+			json.dumps(summary, out)
+
+
+	def output(self):
+		return luigi.LocalTarget(test.path / 'evaluation.json')
+
+
+
+def	test_path(session, test):
+	return session / test.dir / ('debug' if test.debug else 'nodebug')
+
 
 
 class EndpointTestsSummary(luigi.Task):
 	session = luigi.parameter.OptionalPathParameter(default='/tmp/robust_tests/'+str(datetime.datetime.utcnow()).replace(' ', '_'))
+	robust_server_url = luigi.parameter.OptionalParameter(default='http://localhost:8080')
 
-
-	def required_evaluations(self):
-		source = pathlib.Path('../endpoint_tests')
-		dirs = sorted(filter(lambda x: x.is_dir(), source.glob('*/*/')))
-
-		for dir in dirs:
-			yield Evaluation({source:test_dir=dir)
 
 	def requires(self):
 		return list(self.required_evaluations())
 
+
+	def required_evaluations(self):
+		suite = pathlib.Path('../endpoint_tests')
+		dirs = sorted(filter(lambda x: x.is_dir(), suite.glob('*/*/')))
+
+		for dir in dirs:
+			for debug in [False, True]:
+				yield Evaluation({
+					suite:suite,
+					dir:dir,
+					path: test_path(self.session, self.test),
+					debug: debug
+				})
+
+
 	def run(self):
-		print('ok')
+		with self.output().open('w') as out:
+			summary = []
+			for evaluation_file in self.input():
+				evaluation = json.load(evaluation)
+				summary += evaluation
+			json.dumps(summary, out)
+
 
 	def output(self):
-		casesdir = casesdir_(workdir)
-		return luigi.LocalTarget(casesdir)
-
-
-
-		
-		
-Z
+		return luigi.LocalTarget(session / 'summary.json')
 
 
 
 
-tasks:
 
-	`endpoint tests results`
-		parameters
-			robust_server_url: str, default is http://localhost:8080
-			endpoint_tests_dir: Path, default is "./endpoint_tests"
+
 		
 		dependencies
 			`list of available endpoint_tests`
