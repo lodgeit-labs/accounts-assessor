ad788cfec67cca4dbff3427cdeff96c4ffa096f8
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-03-10 22:46:14 +0100

    wait-for-it needs strictly a hostname + port

--- sources/workers/start.sh
+++ sources/workers/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 ../wait-for-it/wait-for-it.sh $RABBITMQ_URL -t 0
-../wait-for-it/wait-for-it.sh remoulade-api:5005 -t 0
+../wait-for-it/wait-for-it.sh $(echo "$REMOULADE_API" | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\)\(:\([0-9]\{1,5\}\)\)\?.*/\2\3/") -t 0
 
 
 _term() {
