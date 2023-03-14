936ce00e789698b9236cc6de31969e4aa8a0c508
koo5 <kolman.jindrich@gmail.com>
2023-03-15 00:59:20 +0100

    setup fixes

--- docker_scripts/first_run.sh
+++ docker_scripts/first_run.sh
@@ -3,15 +3,18 @@
 set DIR (dirname (readlink -m (status --current-filename)))
 cd "$DIR"
 
-sudo apt install python3 python3-pip docker fish apache2-utils
+sudo apt install python3 python3-pip docker fish apache2-utils tmux 
 
 # for building and running containers
-python3 -m pip install click pyyaml 
+python3 -m pip install --user click pyyaml 
 
 # for run_last_request_in_docker
-python3 -m pip install docker 
+python3 -m pip install --user docker 
 
-python3 setup.py develop 
+# for nice building UI
+python3 -m pip install --user libtmux
+
+python3 setup.py develop --user
 
 _ROBUST_COMPLETE=source_fish robust > ~/.config/fish/completions/robust-complete.fish
 
