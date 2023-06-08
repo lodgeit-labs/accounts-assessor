96d6a99e679e53d5d072e61695407a1178c8ed81
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-06-08 17:02:26 +0200

    ..

--- docker_scripts/first_run.sh
+++ docker_scripts/first_run.sh
@@ -3,22 +3,27 @@
 set DIR (dirname (readlink -m (status --current-filename)))
 cd "$DIR"
 
-sudo apt install python3 python3-pip
+
+cat ~/robust_first_run_v1_done.flag; and exit 0
+touch robust_first_run_v1_done.flag
+
+
+sudo apt install -y python3 python3-pip
 
 # not sure if still needed, maybe was only used for generating password file?
 #sudo apt install apache2-utils
 
 # sorry, i do a lot of small scripting with it, as it's my daily shell. But it shouldn't be hard to get by without it, if necessary
-sudo apt install fish 
+sudo apt install -y fish 
 
 # for nice building UI
-sudo apt install tmux
+sudo apt install -y tmux
 
 # you'll definitely need docker obtained one way or another
-sudo apt install docker.io golang-docker-credential-helpers
+which docker; or sudo apt install docker.io golang-docker-credential-helpers
 
 # compose is a tad more suitable for development than swarm 
-sudo apt install docker-compose
+which docker-compose; or sudo apt install -y docker-compose
 
 # for the building and running UI
 python3 -m pip install --user click pyyaml 
@@ -31,12 +36,6 @@ python3 -m pip install --user docker
 
 # for nice building UI
 python3 -m pip install --user libtmux fire
-
-# finally, the building/running script itself. And no, there probably isnt that much reason for it not to be runnable without installation
-python3 setup.py develop --user
-
-_ROBUST_COMPLETE=source_fish robust > ~/.config/fish/completions/robust-complete.fish
-
 
 # if not using compose:
 #docker swarm init
--- docker_scripts/first_run2.sh
+++ docker_scripts/first_run2.sh
@@ -3,6 +3,12 @@
 set DIR (dirname (readlink -m (status --current-filename)))
 cd "$DIR"
 
+# finally, the building/running script itself. And no, there probably isnt that much reason for it not to be runnable without installation
+python3 setup.py develop --user
+
+_ROBUST_COMPLETE=source_fish robust > ~/.config/fish/completions/robust-complete.fish
+`                                 
+
 cd ..
 cp -r secrets_example secrets
 cp -r sources/config_example sources/config
