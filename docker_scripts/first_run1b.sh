314ffe64ad60c5bbbbdf1fa7c76e75589295570b
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-06-08 18:42:17 +0200

    hmm...!

--- docker_scripts/first_run1b.sh
+++ docker_scripts/first_run1b.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env fish
+function _; or status --is-interactive; or exit 1; end
 
 # for the building and running UI
-python3 -m pip install --user click pyyaml 
-
-# (note we're not installing some bindings here, we're installing docker-compose itself)
-# python3 -m pip install --user docker-compose
-
+python3 -m pip install --user click pyyaml;_
+# (note we're not installing bindings here, we're installing docker-compose itself)
+# python3 -m pip install --user docker-compose;_
 # for run_last_request_in_docker (these are bindings)
-python3 -m pip install --user docker 
-
+python3 -m pip install --user docker;_
 # for nice building UI
-python3 -m pip install --user libtmux fire
+python3 -m pip install --user libtmux fire;_
 
--- docker_scripts/first_run2.sh
+++ docker_scripts/first_run2.sh
@@ -4,10 +4,13 @@ set DIR (dirname (readlink -m (status --current-filename)))
 cd "$DIR"
 
 # finally, the building/running script itself. And no, there probably isnt that much reason for it not to be runnable without installation
-python3 setup.py develop --user
+python3 setup.py develop --user;_
 
 _ROBUST_COMPLETE=source_fish robust > ~/.config/fish/completions/robust-complete.fish
 
 cd ..
-cp -r secrets_example secrets
-cp -r sources/config_example sources/config
+cp -r secrets_example secrets;_
+cp -r sources/config_example sources/config;_
+
+
+
