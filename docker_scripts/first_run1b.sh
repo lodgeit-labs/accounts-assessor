b01182ec60d46ff6877ede828213f9601f67b59f
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-06-17 02:09:15 +0200

    fixed python dependency syntax?

--- docker_scripts/first_run1b.sh
+++ docker_scripts/first_run1b.sh
@@ -2,11 +2,11 @@
 function _; or status --is-interactive; or exit 1; end
 
 # for the building and running UI
-python3 -m pip install --user click@8 pyyaml  ;_
+python3 -m pip install --user click>=8 pyyaml  ;_
 # (note we're not installing bindings here, we're installing docker-compose itself)
-docker-compose; or python3 -m pip install --user docker-compose@1.29  ;_
+docker-compose; or python3 -m pip install --user docker-compose>=1.29  ;_
 # for run_last_request_in_docker (these are bindings)
 python3 -m pip install --user docker  ;_
-# for nice building UI
+# for "nice" deployment log following
 python3 -m pip install --user libtmux fire  ;_
 
