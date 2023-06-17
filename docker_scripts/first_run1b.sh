31f121d7369207fcc8c723a1df5cb6df34c29aa7
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-06-17 03:33:43 +0200

    lol

--- docker_scripts/first_run1b.sh
+++ docker_scripts/first_run1b.sh
@@ -2,7 +2,7 @@
 function _; or status --is-interactive; or exit 1; end
 
 # for the building and running UI
-python3 -m pip install --user click>=8 pyyaml  ;_
+python3 -m pip install --user "click>=8" pyyaml  ;_
 # (note we're not installing bindings here, we're installing docker-compose itself)
 docker-compose version; or python3 -m pip install --user docker-compose>=1.29  ;_
 # for run_last_request_in_docker (these are bindings)
