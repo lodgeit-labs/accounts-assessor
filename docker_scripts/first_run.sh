486b7292b09e4b37d83aaf640dd55f9b2ad99b29
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-06-17 02:19:17 +0200

    ugh!

--- docker_scripts/first_run.sh
+++ docker_scripts/first_run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-cat ~/robust_first_run_v1_done.flag && and exit 0
+cd "${0%/*}" # https://stackoverflow.com/questions/3349105/how-can-i-set-the-current-working-directory-to-the-directory-of-the-script-in-ba
+
+cat ~/robust_first_run_v1_done.flag && exit 0
 
 ./first_run0.sh
 ./first_run1.sh
