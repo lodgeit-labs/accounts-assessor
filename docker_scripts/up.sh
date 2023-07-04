1d3f555f1a9ea2f5c4d06cf464aa5eb214943a99
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-07-05 00:29:25 +0200

    ..

--- docker_scripts/up.sh
+++ docker_scripts/up.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env fish
 
+function _; or status --is-interactive; or exit 1; end
+
 # rdf hackery2:fish/get_script_dir.fish
 set DIR (dirname (readlink -m (status --current-filename))); cd "$DIR"
 
-. venv/bin/activate.fish
+. venv/bin/activate.fish ;_
 
 ./up.py $argv
