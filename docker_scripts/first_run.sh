4c40fac0090fc5fb46a858d9b51e6f026ab803a4
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-07-05 00:29:58 +0200

    robust_first flag version bump

--- docker_scripts/first_run.sh
+++ docker_scripts/first_run.sh
@@ -5,7 +5,7 @@ set -e
 
 cd "$(dirname "$(readlink -f -- "$0")")"
 
-FLAG=~/robust_first_run_v6_done.flag
+FLAG=~/robust_first_run_v7_done.flag
 
 cat $FLAG && { echo "not initing again"; exit 0; }
 
