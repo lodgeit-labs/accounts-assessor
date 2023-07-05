1655bf2fff5f6ce6079170c7e11486067432e286
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-07-05 12:27:32 +0200

    correct-er way to activate venv

--- docker_scripts/first_run1b.sh
+++ docker_scripts/first_run1b.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 function _; or status --is-interactive; or exit 1; end
 
-virtualenv -p /usr/bin/python3.10 venv ;_
+python3.10 -m venv venv ;_
 . venv/bin/activate.fish ;_
 
 # (note we're not installing bindings here, we're installing docker-compose itself)
