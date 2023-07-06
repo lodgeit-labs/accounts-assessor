d44ecc5794350ce2ca83101b0b385533f13b6191
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-07-06 15:06:24 +0200

    anyway...

--- sources/frontend/init_venv.sh
+++ sources/frontend/init_venv.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env fish
 
 function _; or status --is-interactive; or exit 1; end
 
-virtualenv -p /usr/bin/python3 venv ;_
-. ./venv/bin/activate ;_
+virtualenv -p /usr/bin/python3.10 venv ;_
+. ./venv/bin/activate.fish ;_
 
-python3 -m pip install --no-cache-dir -r requirements.txt ;_
+python3 -m pip install --no-cache-dir -r requirements-dev.txt ;_
 python3 -m pip install --no-cache-dir -e ../common/libs/remoulade/[rabbitmq,redis,postgres] ;_
