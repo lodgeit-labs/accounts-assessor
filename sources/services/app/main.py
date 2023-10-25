201aefff9035cc1f54b46a556417dfe4ea16a22b
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-10-25 12:43:25 +0200

    more logging

--- sources/services/app/main.py
+++ sources/services/app/main.py
@@ -27,7 +27,9 @@ from . import div7a
 @app.post("/div7a")
 def post_div7a(loan_summary_request: dict):
 	log.warn(loan_summary_request)
-	return dict(result=div7a.div7a_from_json(loan_summary_request['data'], loan_summary_request['tmp_dir_path']))
+	result = dict(result=div7a.div7a_from_json(loan_summary_request['data'], loan_summary_request['tmp_dir_path']))
+	log.warn(result)
+	return result
 
 @app.post("/arelle_extract")
 def post_arelle_extract(taxonomy_locator: str):
