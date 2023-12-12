6a4be2504b55ca0f7d92cba0696a178dea8e5b04
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-12-12 18:39:09 +0100

    only write .htaccess for not-nobody. updated healthcheck command in Dockerfile_hollow

--- sources/common/tmp_dir_path.py
+++ sources/common/tmp_dir_path.py
@@ -52,19 +52,18 @@ def create_tmp_for_user(user):
 
 
 def write_htaccess(user, path):
-	"""
-	"""
 
 	#i think we'll eventually replace the role of apache here, with python.
 	#https://stackoverflow.com/questions/71276790/list-files-from-a-static-folder-in-fastapi
+
 	with open(P(path) / '.access', 'w') as f:
 		f.write(f"""{user}\n""")
-
 	
 	user = re.escape(user)
-	
-	with open(P(path) / '.htaccess', 'w') as f:
-		f.write(f"""
+
+	if user != 'nobody':
+		with open(P(path) / '.htaccess', 'w') as f:
+			f.write(f"""
 
 RewriteEngine On
 
