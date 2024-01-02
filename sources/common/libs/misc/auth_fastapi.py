1a93ce3d59f1b1005d6297d74f18d6506822fc17
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-01-02 02:37:34 +0100

    ..

---
+++ sources/common/libs/misc/auth_fastapi.py
@@ -0,0 +1,28 @@
+from fastapi import Request
+import logging
+logger = logging.getLogger(__name__)
+
+"""
+the header that Caddy sends is CaddyBasicAuthUser, configured in Caddyfile.
+the header that oauth2-proxy sends are X-Forwarded-Email etc.
+
+"""
+
+def get_user(request: Request):
+
+	# get user from header coming from oauth2-proxy
+	authorization = request.headers.get('X-Forwarded-Email', None)
+	logger.info('X-Forwarded-Email: %s' % authorization)
+	authorization = request.headers.get('X-Forwarded-User', None)
+	logger.info('X-Forwarded-User: %s' % authorization)
+	if authorization is not None:
+		return authorization
+
+	# get user from header coming from caddy
+	authorization = request.headers.get('CaddyBasicAuthUser', None)
+	logger.info('CaddyBasicAuthUser: %s' % authorization)
+	if authorization is not None:
+		return authorization + '@basicauth'
+
+	return 'nobody'
+
