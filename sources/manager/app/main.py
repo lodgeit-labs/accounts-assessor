d74576d4e33f4b64385aa4b5040a6b9a5c398d83
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-01-05 02:52:37 +0100

    manager thread interactions sketch..

--- sources/manager/app/main.py
+++ sources/manager/app/main.py
@@ -7,6 +7,7 @@ import os, sys
 import threading
 import time
 
+
 from remoulade import get_broker, Worker
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../common/libs/misc')))
 from tasking import remoulade
@@ -14,7 +15,51 @@ from tasking import remoulade
 from fastapi import FastAPI, Request, File, UploadFile, HTTPException, Form, status, Query, Header
 
 
-from manager_actors import local_rpc, local_calculator
+workers = defaultdict(stack)
+
+def do_job(job):
+	"""
+	runs in remoulade actor thread.
+	two options here:
+	1) worker is trusted, it is running in compose/stack. It repeatedly connects to manager and asks for jobs. There is no need to spawn it, it is already running.
+	2) worker is not trusted, it will be spawned as a fly.io machine here.
+	at either case, we first wait for worker to register with Manager's FastAPI endpoint.
+	"""
+
+	org = job['options']['org']
+
+	if fly:
+		# spawn fly.io machine.
+		fly_machine = requsts.post('https://api.fly.io/v6/apps/robust/instances', json={
+		# If it does not come up in reasonable time, something went wrong.
+		timeout=100
+	else:
+		# assume a worker is already running in compose/stack, and will register when it's free
+		timeout=None
+
+	try:
+		worker = workers['org'].pop(timeout=timeout)
+		worker['toworker'].append(job)
+		while True:
+			if len(worker['fromworker']) == 1:
+				return worker['fromworker'].pop()
+			elif len(worker['fromworker']) > 1:
+				raise Exception('worker returned more than one result')
+			time.sleep(10)
+			if time.now() - worker['lastseen'] > 60 * 10:
+				raise Exception('worker timed out')
+	finally:
+		if fly:
+			fly_machine.delete()
+
+
+
+# not sure how to compose this cleanly
+import manager_actors
+manager_actors.do_job = do_job
+
+
+
 def start_worker2():
 	"""
 	this is a copy of remoulade.__main__.start_worker that works inside a thread
@@ -53,3 +98,36 @@ app = FastAPI(
 	summary="invoke accounting calculators and other endpoints",
 )
 
+@app.post("/connect")
+def connect(org: str):
+	"""
+	worker calls this to register itself with manager.
+	"""
+
+	w = Worker(
+			toworker = fifo(),
+			fromworker = fifo(),
+			lastseen = time.now()
+	)
+
+	workers[org].push(w)
+
+	# now, if we'll be able to keep the connection open for the whole time:
+
+	while True:
+		msg = w.toworker.pop()
+		# no timeout. assume that the 'finally' block in do_job works, so, no matter what, if an actor dies, the machine is freed. Trusted workers will simply re-initiate connection.
+		# we can have another script sweep the flyio machines periodically, just in case
+		request.send(json.dumps(msg))
+		hmmm
+
+
+
+
+
+
+
+
+
+
+	return 'ok'
--- sources/manager/app/manager_actors.py
+++ sources/manager/app/manager_actors.py
@@ -3,6 +3,7 @@
 frontend (or other caller) imports this file.
 
 """
+import time
 
 import remoulade
 
@@ -21,25 +22,39 @@ def local_calculator(
 	worker_options=None,
 	request_format=None
 ):
+
+	# this is gonna call out to workers and wait for them to finish. But even more properly,
+	# local_calculator actor should be composed with convert_request_file actors, and the whole pipeline should be managed on remoulade level, with retries and everything. Here, if prolog fails, we'll be redoing the conversions too, and there'll be no visibility into it.
+
+	converted_request_files = convert_request_files(files_in_dir(get_tmp_directory_absolute_path(request_directory)))
+
 	msg = dict(
 		method='calculator',
 		params=dict(
 			request_format=request_format,
 			request_tmp_directory_name = request_directory,
-			request_files = convert_request_files(files_in_dir(get_tmp_directory_absolute_path(request_directory))),
+			request_files = converted_request_files,
 			public_url = public_url
 		)
 	)
+
+	# for trusted workers, we can keep the tmp dir logic as is. Tmp dir is created inside worker, etc.
+	# but for untrusted workers, the machine will be started with request files (somehow), and we'll need to fetch the results from the machine when it's done. We can still stick to the shared volume etc, but we have to create the tmp dir here on manager, and copy the files to the machine, and fetch back the result, and symlink from final_result here..
+	# i can eventually see the "calculator" function being completely collapsed from worker into manager here.
+
 	update_last_request_symlink(request_directory)
-	return call_prolog_calculator(msg=msg, worker_options=worker_options)
 
-
-remoulade.declare_actors([local_rpc, local_calculator])
-
-
+	return do_job(dict(
+		proc='call_prolog_calculator',
+		msg=msg,
+		worker_options=worker_options))
 
 
 def _call_prolog(msg, options):
-	
+	return do_job(dict(
+		msg=msg,
+		options=options
+	))
 
 
+remoulade.declare_actors([local_rpc, local_calculator])
