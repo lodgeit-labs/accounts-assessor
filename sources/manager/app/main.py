21cb4619470084678d108c6c22929974c65962b2
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-01-06 03:26:53 +0100

    hmm...!

--- sources/manager/app/main.py
+++ sources/manager/app/main.py
@@ -6,7 +6,7 @@ import logging
 import os, sys
 import threading
 import time
-
+from collections import defaultdict
 
 from remoulade import get_broker, Worker
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../common/libs/misc')))
@@ -99,35 +99,118 @@ app = FastAPI(
 )
 
 @app.post("/connect")
-def connect(org: str):
+def connect(org: str, id: str):
 	"""
 	worker calls this to register itself with manager.
+	This form should only be allowed from trusted workers, because untrusted workers should be required to send a jwt instead of passing these parameters plaintext
 	"""
 
 	w = Worker(
+			id = id,
+			org = org,
 			toworker = fifo(),
 			fromworker = fifo(),
 			lastseen = time.now()
 	)
 
-	workers[org].push(w)
+	worker_registrator_queue.push(w)
 
-	# now, if we'll be able to keep the connection open for the whole time:
 
+
+@app.post("/messages")
+def messages(org: str, id: str):
+	return(toworker[id].pop())
+
+
+def result(org: str, id: str, result):
+	fromworker[id].push(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pending_jobs = []
+
+def synchronization_thread():
 	while True:
-		msg = w.toworker.pop()
-		# no timeout. assume that the 'finally' block in do_job works, so, no matter what, if an actor dies, the machine is freed. Trusted workers will simply re-initiate connection.
-		# we can have another script sweep the flyio machines periodically, just in case
-		request.send(json.dumps(msg))
-		hmmm
+		e = events.pop()
+		event(e)
+
+def event(e):
+	if e.type == 'job_result':
+		e.worker.job = None
+		e.worker.fromworker.push(message) # actor pops it
+
+	if msg.type == 'worker_asks_for_job':
+		worker = msg.worker
+		if worker.job:
+			worker.fromworker.push(dict(type='failure', reason='worker reset'))
+		for job in pending_jobs:
+			if match(job, worker):
+				worker.job = job
+				worker.toworker.push(job)
+				break
+
+	elif msg.type == 'add_job':
+		for worker in workers:
+			if worker.job is None and match(msg.job, worker):
+				worker.job = job
+				worker.toworker.push(msg.job)
+				break
+		else:
+			pending_jobs.append(msg.job)
 
 
 
 
+def match(job, worker):
+	return job.org == worker.org and job.size in worker.sizes
 
 
 
+workers = []
+pending_jobs = []
 
 
+def get_worker(id, org):
+	worker = workers.get(id)
+	if worker is None:
+		worker = Worker(
+			id = id,
+			org = org,
+			toworker = fifo(),
+			fromworker = fifo(),
+			lastseen = time.now()
+		)
+		workers[id] = worker
+	return worker
 
-	return 'ok'
+
+@app.port("/worker_sends_message")
+def worker_sends_message(org: str, id: str, message):
+	worker = get_worker(id, org)
+	synchronization_queue.push(dict(worker=w, message=message))
+
