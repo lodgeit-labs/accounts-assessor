5fbd004711361b7b62995b84c33aecbbe77c0628
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-01-10 00:52:23 +0100

    need to think through tmp dir distribution to workers..

--- sources/worker/app/main.py
+++ sources/worker/app/main.py
@@ -1,8 +1,8 @@
+"""
+a single image 
 
 
-
-
-
+"""
 
 app = FastAPI(
 	title="Robust worker private prolog facing helper api"
@@ -10,17 +10,17 @@ app = FastAPI(
 
 def manager_proxy_thread():
 	while True:
-		r = requests.post(os.environ['MANAGER_URL'] + '/messages', json=dict(org=org, id=id, procs=['call_prolog', 'arelle', 'download']))
+		r = requests.post(os.environ['MANAGER_URL'] + '/messages', json=dict(id=id, procs=['call_prolog', 'arelle', 'download']))
 		r.raise_for_status()
 		msg = r.json()
 
 		if msg['type'] == 'job':
 			if msg['proc'] == 'call_prolog':
-				return call_prolog(msg['msg'], msg['options'])
+				return call_prolog(msg['msg'], msg['worker_options'])
 			elif msg['proc'] == 'arelle':
-				return arelle(msg['msg'], msg['options'])
+				return arelle(msg['msg'], msg['worker_options'])
 			elif msg['proc'] == 'download':
-				return download(msg['msg'], msg['options'])
+				return download(msg['msg'], msg['worker_options'])
 			else:
 				raise Exception('unknown proc: ' + msg['proc'])
 
