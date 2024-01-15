4e5712eee12577a91a13acaa4a152ffd706689aa
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-01-15 05:08:00 +0100

    fixups

--- sources/worker/start.sh
+++ sources/worker/start.sh
@@ -3,9 +3,9 @@
 set -x
 
 if [ ! -z $WATCHMEDO ]; then
-  watchmedo auto-restart --debounce-interval 1 --interval $WATCHMEDO_INTERVAL -d .  -d ../common  --patterns="*.py;*.egg" --recursive  --  start2.sh
+  watchmedo auto-restart --debounce-interval 1 --interval $WATCHMEDO_INTERVAL -d .  -d ../common  --patterns="*.py;*.egg" --recursive  --  ./start2.sh
 else
-  start2.sh
+  ./start2.sh
 fi
 
 echo ".process end ======================================================================= end process ."
