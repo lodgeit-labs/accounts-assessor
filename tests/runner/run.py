01073d9c6b277fd1161387efd91646f0d6a47f4f
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-01-30 11:25:09 +0100

    div7a..

--- tests/runner/run.py
+++ tests/runner/run.py
@@ -19,6 +19,8 @@ def run(server):
 	
 
 
+
+
 	r = requests.post(server + '/chat/', json=dict(
 		{
 			"method": "chat",
@@ -38,24 +40,29 @@ def run(server):
 	
 	r = json.loads(co("""curl -X 'GET' \
   server + '/ai3/div7a?loan_year=2020&full_term=7&opening_balance=100000&opening_balance_year=2020&lodgement_date=2021-06-30&repayment_dates=2021-06-30&repayment_dates=2022-06-30&repayment_dates=2023-06-30&repayment_amounts=10001&repayment_amounts=10002&repayment_amounts=10003' -H 'accept: application/json'"""))
+
 	del r['details_url']
+
 	if r == {}:
 		pass
 	else:
 		log.error('bad response: %s', r)
 		sys.exit(1)
-	
+
+
 	
 	
 	
 	# this still requires scheduler server and helper workers to be running
 	# ./luigi_scheduler_server.sh
 	# ./luigi_worke.sh
+
 	co(f'PYTHONPATH=(pwd) luigi --module runner.tests2 --Permutations-robust-server-url {server} --Permutations-suite ../endpoint_tests/ --workers=30 Summary')
 
-
-
-
-
-
-	log.info('ok.')
+	"""
+	evaluation is required here.
+	we should either run the worker through python here, 
+	or come up with session name,
+	and then check the results.
+	"""
+	#log.info('ok.')
