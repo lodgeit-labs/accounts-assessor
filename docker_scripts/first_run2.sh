efa33a7b18f685c7fbe021bd0cb85ee7a9cfe64f
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-02-03 13:25:37 +0100

    definitely use e instead of _ here.

--- docker_scripts/first_run2.sh
+++ docker_scripts/first_run2.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env fish
+function e; or status --is-interactive; or exit 1; end
 set DIR (dirname (readlink -m (status --current-filename))); cd "$DIR"
-function _old_fish_prompt; end; # https://github.com/python/cpython/issues/93858 ?
+#function _old_fish_prompt; end; # https://github.com/python/cpython/issues/93858 ?
 
 set VENV_PATH ./venv
-. $VENV_PATH/bin/activate.fish ;_
+. $VENV_PATH/bin/activate.fish ;e
 
-python3 setup.py develop ;_
+python3 setup.py develop ;e
 
 #mkdir -p ~/.config/fish/completions/
 _ROBUST_COMPLETE=source_fish robust > ./venv/robust-complete.fish
