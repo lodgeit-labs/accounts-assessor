ed784b2fed8731bd7c87936ceb02d15e9b052b4f
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-02-04 16:31:22 +0100

    ..

--- docker_scripts/up.sh
+++ docker_scripts/up.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env fish
 function _; or status --is-interactive; or exit 1; end
 set DIR (dirname (readlink -m (status --current-filename))); cd "$DIR"
-function _old_fish_prompt; end; # https://github.com/python/cpython/issues/93858 ?
+#function _old_fish_prompt; end; # https://github.com/python/cpython/issues/93858 ?
 
 
 ./first_run.sh
 
-set VENV_PATH ~/.local/robust/$DIR/venv
+set VENV_PATH ./venv
 . $VENV_PATH/bin/activate.fish ;_
 
 PYTHONUNBUFFERED=true ./up.py $argv
