743052679c568dbc2cbe41f6b5781f42c436496a
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-03-04 22:38:42 +0100

    ..

--- sources/common/libs/misc/auth_fastapi.py
+++ sources/common/libs/misc/auth_fastapi.py
@@ -1,7 +1,15 @@
 from fastapi import Request
 import logging
+
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 
 """
--- sources/frontend/start2.sh
+++ sources/frontend/start2.sh
@@ -2,6 +2,6 @@
 
 PYTHONPATH=../common/libs/remoulade/
 
-python3 -O `which uvicorn` app.main:app --proxy-headers --host 0.0.0.0 --port 7788  --workers 8 --log-level info #debug
+python3 -O `which uvicorn` app.main:app --proxy-headers --host 0.0.0.0 --port 7788  --workers 8 --log-level debug
 
 #python3 -O `which gunicorn` app/main.py  --proxy-headers --bind 0.0.0.0:7788 --workers 4 --worker-class uvicorn.workers.UvicornWorker --log-level trace $@
